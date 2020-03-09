3193ab0b1318c4c00453cc8f7c6a670098cadd2b
wang <1156595708@qq.com>
2020-03-09 11:32:56 +0800

    增加图片上传

---
+++ src/main/java/life/wl/community/controller/FileController.java
@@ -0,0 +1,18 @@
+package life.wl.community.controller;
+
+import life.wl.community.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload(){
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/weibo-wechat.png");
+        return fileDTO;
+    }
+}
